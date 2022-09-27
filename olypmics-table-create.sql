9b0b88e137d29ca4947119945a4fec278d43d900
bobpickerel <bob.pickerel@gmail.com>
2022-09-27 19:58:30 -0400

    added create.sql script for database

---
+++ olypmics-table-create.sql
@@ -0,0 +1,31 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+-- Olympic Database
+
+CREATE TABLE "athlete_events" (
+    "athlete_id" INT   NOT NULL,
+    "name" VARCHAR   NOT NULL,
+    "sex" VARCHAR   NOT NULL,
+    "age" VARCHAR,
+    "height"  VARCHAR,
+    "weight" VARCHAR,
+    "team" VARCHAR   NOT NULL,
+    "noc" VARCHAR   NOT NULL,
+    "games" VARCHAR   NOT NULL,
+    "year" INT   NOT NULL,
+    "season" VARCHAR   NOT NULL,
+    "city" VARCHAR   NOT NULL,
+    "sport" VARCHAR   NOT NULL,
+    "event" VARCHAR   NOT NULL,
+    "medal" VARCHAR   NOT NULL,
+    "lat" VARCHAR   NOT NULL,
+    "lng" VARCHAR   NOT NULL,
+    "noc_country" VARCHAR   NOT NULL,  
+    "country_lat" VARCHAR   NOT NULL,
+    "country_long" VARCHAR   NOT NULL
+
+);
+
+
+
