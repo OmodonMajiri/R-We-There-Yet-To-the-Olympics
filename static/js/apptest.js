c78ba68df8f3a93359bae4f24d02e51e01a6192f
VedranaGATech <vedranab79@gmail.com>
2022-10-03 20:37:16 -0500

    index update

--- static/js/apptest.js
+++ static/js/apptest.js
@@ -1,4 +1,22 @@
 
+
+//initialize demographics dashboard "athletesDataset"
+function initialize(){
+    //drop down selector
+    var select = d3.select("#athletesDataset");
+    d3.json("/api/v1.0/athlete/<sex>/demographic/<year>").then((data) => {
+        let athleteName = data; //array of events
+        //console.log(eventNames);
+        //use a for each to create options
+        athleteName.forEach((name) => {
+            select.append("option")
+                .text(name)
+                .property("value", name);
+         });
+         let firstSample = athleteName[0];
+     });
+}
+
 // initialize dashboard
 function initialize(){
     //drop down selector
