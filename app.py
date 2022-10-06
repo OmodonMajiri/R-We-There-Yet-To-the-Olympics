d2a526db20662f3db90f31fcdcd635940200c389
VedranaGATech <vedranab79@gmail.com>
2022-10-05 19:01:35 -0500

    index file update

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ app = Flask(__name__)
 # Flask Routes
 #################################################
 
-@app.route("/index")
+@app.route("/")
 def index():
     return render_template('index.html')
 
